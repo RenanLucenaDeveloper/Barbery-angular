10d60e700bd5b905fb5fcec7d81914bbaaca1889
Renan Lucena <reenandeveloper@hotmail.com>
2023-03-22 18:10:50 -0300

    adicionando ao github-pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./scss/app.component.scss', './scss/partes/_header.scss', './scss/partes/_intro.scss', './scss/partes/_sobre.scss', './scss/partes/_contato.scss', './scss/partes/_footer.scss', './scss/partes/_modal.scss', './scss/partes/_mobile.scss']
 })
 export class AppComponent {
-  title = 'barbery-angular';
+
+  //Seleciona o container do modal
+    @ViewChild('modalContainer')
+    modalContainer?:ElementRef;
+
+  //Seleciona o modal
+  @ViewChild('modal')
+  modal?:ElementRef;
+  
+  toggleModal(event : Event) {
+    setTimeout(() => {
+
+      if(event.target instanceof HTMLElement && (event.target?.dataset['modal'] === 'container' || event.target?.dataset['modal'] === 'toggle')) {
+
+        console.log(event.target?.classList[0]);
+
+        this.modalContainer?.nativeElement.classList.toggle('ativo');
+      }
+
+    }, 0)
+  }
 }
